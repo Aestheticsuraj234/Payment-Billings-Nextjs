1f7bd45a002c632c09fd4a4aa84b9286b0fd669b
Aestheticsuraj234 <jhapriya458@gmail.com>
2025-10-28 12:41:27 +0530

    04 razorpay integration

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
+import Script from "next/script";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,6 +25,11 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+          <Script
+                  id="razorpay-checkout-js"
+                  src="https://checkout.razorpay.com/v1/checkout.js"
+                  strategy="lazyOnload"
+                />
         {children}
         <Toaster/>
       </body>
